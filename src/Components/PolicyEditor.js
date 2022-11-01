f7a64c9ae483effdfcc4bae74c8ae333ed4e7532
John Vick <wick14064@gmail.com>
2022-11-01 19:42:03 +0500

    Updated Violation Heading

--- src/Components/PolicyEditor.js
+++ src/Components/PolicyEditor.js
@@ -52,7 +52,7 @@ export default function PolicyEditor() {
     <div className='p-2' style={{background: '#DCDCDC', borderColor: '#707070',height:'82vh'}}>
         <div className='container' style={{background: '#A19B9E', border: '1px solid #707070', borderRadius:'5px', height:'80vh'}}>
             <div className="row p-0 m-0 justify-content-between">
-                <h1 className='col-2' style={{marginBottom: '50px', marginTop: '10px', color: 'white', paddingLeft: '12px', margin: '0', fontSize: '29px', textDecoration: 'underline'}}>
+                <h1 className='col-2 p-0 pt-2 ms-3' style={{borderBottom: '1px solid white', marginBottom: '50px', marginTop: '10px', color: 'white', paddingLeft: '12px', margin: '0', fontSize: '29px'}}>
                     Policies
                 </h1>
                 <div className='col-1 ps-3 py-1'>
@@ -63,7 +63,7 @@ export default function PolicyEditor() {
             </div>
 
             <div className='container my-3'>    
-                <div className='row my-2 mx-1 p-0 py-2' style={{color: 'white'}}>
+                <div className='row my-0 mx-1 p-0 py-1' style={{color: 'white'}}>
                     
                     <div className='col-2 text-center'>
                         Activity
