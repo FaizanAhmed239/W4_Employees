57310de69c3ce4d61595a89fe70d80de478493c9
John Vick <wick14064@gmail.com>
2022-11-23 15:40:27 +0500

    backgroundlogo path update

--- src/Components/SystemControl.js
+++ src/Components/SystemControl.js
@@ -4,7 +4,11 @@ export default function SystemControl() {
   return (
     <div className='p-2' style={{background: '#DCDCDC', borderColor: '#707070',height:'82vh'}}>
         <div className='' style={{background: '#A19B9E', border: '1px solid #707070', borderRadius:'5px', height:'80vh'}}>
-            <h1>SystemControl</h1>
+          <div className="row p-0 m-0">
+            <h1 className='col-2 p-0 pt-2 ms-3' style={{marginBottom: '10px', marginTop: '10px', color: 'white', paddingLeft: '12px', margin: '0', fontSize: '29px', borderBottom: '1px solid white'}}>
+              SystemControl
+            </h1>
+          </div>
         </div>
     </div>
   )
--- src/Components/Violations.js
+++ src/Components/Violations.js
@@ -52,7 +52,7 @@ export default function Violations() {
     <div className='p-2' style={{background: '#DCDCDC', borderColor: '#707070',height:'82vh'}}>
         <div className='container' style={{background: '#A19B9E', border: '1px solid #707070', borderRadius:'5px', height:'80vh'}}>
             <div className="row p-0 m-0">
-              <h1 className='col-2 p-0 pt-2 ms-3' style={{marginBottom: '10px', marginTop: '10px', color: 'white', paddingLeft: '12px', margin: '0', fontSize: '29px', borderBottom: '1px solid white'}}>
+              <h1 className='col-2 p-0 pt-2 ms-3' style={{display: 'inline-block', marginBottom: '10px', marginTop: '10px', color: 'white', paddingLeft: '12px', margin: '0', fontSize: '29px', borderBottom: '1px solid white'}}>
                 Violations
               </h1>
             </div>
