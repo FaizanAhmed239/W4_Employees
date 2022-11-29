097577a4cd11a1b4630a3b3f6bc6ef2be67c21f6
John Vick <wick14064@gmail.com>
2022-11-29 15:00:37 +0500

    Update PolicyControl.js

--- src/Components/PolicyControl.js
+++ src/Components/PolicyControl.js
@@ -41,10 +41,10 @@ export default function PolicyControl() {
                             <div className='col-3 text-center'>
                                 {policy.time}
                             </div>
-                            <div className='col-2 text-center'>
+                            <div className='col-2 pe-4 text-center'>
                                 {policy.zone}
                             </div>
-                            <div className='col-2 text-center'>
+                            <div className='col-2 ps-4 text-center'>
                                 {policy.IsAllowed}
                             </div>
                             <div className='col-2 ps-5 text-center'>
@@ -62,17 +62,17 @@ export default function PolicyControl() {
     return (
 
         <div className='container-fluid' style={{ height: '100%' }}>
-            <div className='row mx-0 mt-4 p-0 py-1' style={{ color: 'white', background: '#707070' }}>
-                <div className='col-3 text-center'>
+            <div className='row mx-0 me-2 mt-4 p-0 py-1' style={{ color: 'white', background: '#707070' }}>
+                <div className='col-3 pe-4 text-center'>
                     Activity
                 </div>
                 <div className='col-3 text-center'>
                     Time Duration
                 </div>
-                <div className='col-2 ps-5'>
+                <div className='col-2 px-5'>
                     Zone
                 </div>
-                <div className='col-2 ps-4'>
+                <div className='col-2 px-5'>
                     IsAllowed
                 </div>
                 <div className='col-2 ps-0'>
